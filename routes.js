b855dd20435a6852bdc597c7950f04692b30f218
Desiree Morimoto <dmorimoto89@gmail.com>
2023-11-29 10:51:47 -0800

    Add POST route to create new users

--- routes.js
+++ routes.js
@@ -9,11 +9,18 @@ const Course = require('./models').Course;
 router.get('/users', async (req, res) => {
     const users = await User.findAll();
     res.status(200).json(users);
-})
+});
+
 // Route that returns all courses
 router.get('/courses', async (req, res) => {
     const courses = await Course.findAll();
     res.status(200).json(courses);
-})
+});
+
+// Route to create a new user
+router.post('/users', async(req, res) => {
+    const user = await User.create(req.body);
+    res.status(201).location('/').end();
+});
 
 module.exports = router;
